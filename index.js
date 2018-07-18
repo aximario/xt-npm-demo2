f8d64eb0dcaf743227a7b7f00c9ff0494f0501c7
祥拓 <xiangtuo@pinduoduo.com>
2018-07-18 18:43:10 +0800

    update: 1.0.2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const xtNpm = require('xt-npm-demo')
 
-const version = '1.0.1'
+const version = '1.0.2'
 
 module.exports = `xt-npm-demo[version: ${xtNpm.version}], xt-npm-demo2[version: ${version}]`
